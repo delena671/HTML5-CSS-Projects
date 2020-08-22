e0a690bfc12a79f15d42e4025074fd68eddaa7a3
delena671 <69768942+delena671@users.noreply.github.com>
2020-08-22 14:29:17 -0400

    Add files via upload

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+<script>
+alert("I Love You!")
+</script>
+</html>
+function My_First_Function(){
+var string "Kiss me, i'm Irish";
+var result=string.fontcolor("green");
+documents.getelementbyid("Irish").inner html=
+}
